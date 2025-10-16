03772d1c87da9990445e623c2d3984bfa66e11c2
Prof. Felipe Vilella <proffelipe.oliveira@fiap.com.br>
2025-10-16 08:05:59 -0300

    first commit

---
+++ src/main/java/br/com/fiap/bookish_octo_guacamole/BookishOctoGuacamoleApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.bookish_octo_guacamole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookishOctoGuacamoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookishOctoGuacamoleApplication.class, args);
+	}
+
+}
