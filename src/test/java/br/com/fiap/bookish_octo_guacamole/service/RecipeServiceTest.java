15adc4e317b24a3f753e93ce13e8580aed2cca7e
Prof. Felipe Vilella <proffelipe.oliveira@fiap.com.br>
2025-10-16 08:56:39 -0300

    fix: test assertion

--- src/test/java/br/com/fiap/bookish_octo_guacamole/service/RecipeServiceTest.java
+++ src/test/java/br/com/fiap/bookish_octo_guacamole/service/RecipeServiceTest.java
@@ -1,12 +1,12 @@
 package br.com.fiap.bookish_octo_guacamole.service;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 public class RecipeServiceTest {
   @Test
   void primeiroTest() {
-    assertNotEquals("corinthians", "santos");
+    assertEquals("corinthians", "santos");
   }
 }
