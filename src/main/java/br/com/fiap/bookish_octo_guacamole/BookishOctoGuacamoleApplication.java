2f8d16782dd35309a08e579902d07a0f2497358b
Prof. Felipe Vilella <proffelipe.oliveira@fiap.com.br>
2025-10-16 08:29:29 -0300

    feat: deitando no java

--- src/main/java/br/com/fiap/bookish_octo_guacamole/BookishOctoGuacamoleApplication.java
+++ src/main/java/br/com/fiap/bookish_octo_guacamole/BookishOctoGuacamoleApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookishOctoGuacamoleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookishOctoGuacamoleApplication.class, args);
+		JAVA.run(BookishOctoGuacamoleApplication.class, args);
 	}
 
 }
